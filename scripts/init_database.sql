abd37215e1b0c0f0a44c81670ee6a507298c796d
peterPalVanyo <vanyo.peter.pal@gmail.com>
2025-07-21 14:40:22 +0200

    define gold views

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,21 +1,22 @@
 /*
-========================
-Create Database Schemas
-========================
-Script purpose:
-    this script creates a new database named 'DataWarehouse' after checking if it already exists.
-    if the database exists, it is dropped and recreated. additionally, the script sets up three schemas
-    within the database: bronse, silver and gold.
-Warning:
-    this script will drop the entire 'DataWarehouse' database if exists.
-    all data in the database will be permanently deleted. proceed with caution
-    and ensure you have a proper backups before running this script.
-
+=============================================================
+Create Database and Schemas
+=============================================================
+Script Purpose:
+    This script creates a new database named 'DataWarehouse' after checking if it already exists. 
+    If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas 
+    within the database: 'bronze', 'silver', and 'gold'.
+	
+WARNING:
+    Running this script will drop the entire 'DataWarehouse' database if it exists. 
+    All data in the database will be permanently deleted. Proceed with caution 
+    and ensure you have proper backups before running this script.
 */
 
 USE master;
+GO
 
---Drop and recreate the 'DataWarehouse' database
+-- Drop and recreate the 'DataWarehouse' database
 IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
 BEGIN
     ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
@@ -23,14 +24,19 @@ BEGIN
 END;
 GO
 
---Create Database 'DataWarehouse'
+-- Create the 'DataWarehouse' database
 CREATE DATABASE DataWarehouse;
+GO
 
 USE DataWarehouse;
---Schemas
 GO
+
+-- Create Schemas
 CREATE SCHEMA bronze;
 GO
+
 CREATE SCHEMA silver;
 GO
+
 CREATE SCHEMA gold;
+GO
